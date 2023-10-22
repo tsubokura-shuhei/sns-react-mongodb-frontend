c2a8a1536fb0c1aaf7eeeb91444609bae9691721
tsubokura-shuhei <repeat2639@gmail.com>
2023-10-22 14:01:40 +0900

    material-UI install

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Chat, Notifications, Search } from "@mui/icons-material";
 
 const Topbar = () => {
   return (
@@ -8,12 +9,19 @@ const Topbar = () => {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <Search className="searchIcon" />
           <input type="text" className="searchInput" placeholder="検索" />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconBadge">2</span>
+        </div>
       </div>
       <img src="/assets/person/1.jpeg" alt="TopImage" />
     </div>
