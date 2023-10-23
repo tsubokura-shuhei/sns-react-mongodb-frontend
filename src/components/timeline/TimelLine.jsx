64c143d7cd917889872ddd4139b26982d710ff16
tsubokura-shuhei <repeat2639@gmail.com>
2023-10-23 23:00:32 +0900

    sidebar create

---
+++ src/components/timeline/TimelLine.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./TimeLine.css";
+
+const TimelLine = () => {
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        {/* <Share /> */}
+        {/* <Post /> */}
+      </div>
+    </div>
+  );
+};
+
+export default TimelLine;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "./Home.css";
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import TimelLine from "../../components/timeline/TimelLine";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 const Home = () => {
   return (
     <>
       <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <TimelLine />
+        <Rightbar />
+      </div>
     </>
   );
 };
